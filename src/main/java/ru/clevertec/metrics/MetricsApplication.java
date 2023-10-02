6f7e7bd2d93fdac49eb3554cfa226d6eb305c601
Sergey Bobrovich <sabobrovich@alfaleasing.ru>
2023-10-02 12:03:42 +0300

    Add cache to repository

--- src/main/java/ru/clevertec/metrics/MetricsApplication.java
+++ src/main/java/ru/clevertec/metrics/MetricsApplication.java
@@ -2,7 +2,9 @@ package ru.clevertec.metrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class MetricsApplication {
 
--- src/main/java/ru/clevertec/metrics/repository/ProductRepository.java
+++ src/main/java/ru/clevertec/metrics/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package ru.clevertec.metrics.repository;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import ru.clevertec.metrics.entity.Product;
 
 import java.util.UUID;
@@ -9,5 +13,11 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     @Query("SELECT SUM(count) FROM Product")
+    @Cacheable(cacheNames = "count_products", key = "'count'")
     Long getCountOfAllProducts();
+
+    @Modifying
+    @NonNull
+    @CacheEvict(value = "count_products", key = "'count'")
+    <S extends Product> S save(@NonNull S product);
 }
